2097311fe1377f518e0d8d495674e3e863200459
Ivan Leschinsky <vanopiano@gmail.com>
2022-04-04 13:27:57 +0300

    Add first argument parsing, colorized exit with error (#4)

--- main.go
+++ main.go
@@ -6,17 +6,43 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"errors"
+	"github.com/fatih/color"
 )
 
-func main() {
-	var overrideCommand string
 
-	flag.StringVar(&overrideCommand, "c", "", "command to run on server")
-	flag.Parse()
+func exit(err error) {
+	color.Set(color.FgRed)
+	log.Fatalln(err)
+	os.Exit(1)
+}
+
+func parseArguments() (string, string) {
+	if (len(os.Args) < 2) {
+		exit(errors.New("Expected at least one argument as server"))
+	}
+
+	var overrideCommand string
+	var server string
+	server = os.Args[1]
+
+	if (len(os.Args) > 2) {
+		mySet := flag.NewFlagSet("",flag.ExitOnError)
+		mySet.StringVar(&overrideCommand, "c", "", "command to run on server")
+		mySet.Parse(os.Args[2:])
+	}
+	return server, overrideCommand
+}
+
+func main() {
+	log.SetFlags(0)
+
+	server, overrideCommand := parseArguments()
+	fmt.Println("server to run: ", server)
 
 	cfg, err := readConf("./servers.yml", overrideCommand)
 	if err != nil {
-		log.Fatalln(err)
+		exit(err)
 	}
 
 	args := []string{fmt.Sprintf("%v@%v", cfg.User, cfg.Host)}
